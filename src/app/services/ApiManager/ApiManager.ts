25e21c8a7174f56a356c47624ac257f410543c71
Erwin Mario Leon Ramos <erwin@apoloenergies.es>
2026-06-29 01:49:15 -0400

    refractor: DNI and signer prodcedure

--- src/app/services/ApiManager/ApiManager.ts
+++ src/app/services/ApiManager/ApiManager.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export const ApiManager = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_ENVIRONMENT_LOCAL,
-  baseURL: process.env.NEXT_PUBLIC_ENVIRONMENT_PROD,
-  // baseURL: "https://ee.renovaenergy.es/api/VicoApi",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   responseType: "json",
   withCredentials: true,
 });
